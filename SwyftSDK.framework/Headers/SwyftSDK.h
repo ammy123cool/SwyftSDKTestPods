4dcb72e8d06bd55842124b22bf37677e9ceeb584
Amit Dhawan <amit.dhawan@monotype.com>
2017-02-25 14:45:58 +0530

    - Frmaework release

---
+++ SwyftSDK.framework/Headers/SwyftSDK.h
@@ -0,0 +1,19 @@
+//
+//  SwyftSDK.h
+//  SwyftSDK
+//
+//  Created by Brandon Fitzpatrick on 8/22/16.
+//  Copyright © 2016 Swyft Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwyftSDK.
+FOUNDATION_EXPORT double SwyftSDKVersionNumber;
+
+//! Project version string for SwyftSDK.
+FOUNDATION_EXPORT const unsigned char SwyftSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwyftSDK/PublicHeader.h>
+
+
